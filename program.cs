63b0c6acc5ce96df3a07ac5feea1a9591d80ed6a
Bhavesh N <osi.bhavesh@gmail.com>
2020-04-11 23:36:26 +1000

    Update program.cs
    New update
    
    But I just pass 3 test case only
     
    
    0 0 2 0 0 0 2 0
    0 2 0 0 0 0 2 0
    0 0 0 2 0 0 0 0 
    0 0 2 1 1 0 0 1
    0 2 2 0 2 2 0 1
    0 0 0 0 0 0 0 0
    1 0 1 0 0 0 0 0
    0 1 0 0 0 0 0 0 
    
    
    input (2,2)
    
    output 22  aave but upar na code mujab wrong aave che
    ena pachi 53*53 no array che ema pan wrong aave che
    input (37,40)

--- program.cs
+++ program.cs
@@ -1,107 +1,93 @@
-using System.CodeDom.Compiler;
+using System;
 using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
 
-
-
-class Result
+namespace ConsoleApp1
 {
-
-    /*
-     * Complete the 'minMoves' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts following parameters:
-     *  1. 2D_INTEGER_ARRAY maze
-     *  2. INTEGER x
-     *  3. INTEGER y
-     */
-
-    public static int minMoves(List<List<int>> maze, int x, int y)
+    class Result
     {
-        int inerCount = 0;
-        int outCount=0;
-        int r = 0;
-        int c = 0;
-        bool check=false;
-        Console.WriteLine(maze[1][1]);
-        if(maze[x][y]==1){
-            return -1;
-        }
-        else
+
+        /*
+         * Complete the 'minMoves' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts following parameters:
+         *  1. 2D_INTEGER_ARRAY maze
+         *  2. INTEGER x
+         *  3. INTEGER y
+         */
+
+        
+
+        public static int minMoves(List<List<int>> maze, int x, int y)
         {
-            for(int row=r; row<maze.Count;row++)
+            int count = 0;
+            
+            if (maze[0][1]==1 && maze[1][0]==1)
             {
-                for(int col=c;col<maze[0].Count;col++)
+                return -1;
+            }
+            
+            for (int row = 0; row < maze.Count; row++)
+            {
+                for (int col = 0; col < maze[0].Count; col++)
                 {
-                    inerCount = inerCount+1;
-                    if(row==x && col==y && check==false)
+                    if (col == y)
                     {
-                        Console.WriteLine(maze[row][col]);
-                            if(maze[x][y]==1)
-                            {
-                                return -1;
-                            }
-                            else if(maze[row][col]==2)
-                            {
-                                return inerCount;
-                            }
-                            else
-                            {
-                                r=0;
-                                c=0;
-                                check = true;
-                            }
+                        if (row + 1 == x)
+                        {
+                            count++;
+                            return count;
+                        }
+                        else if (maze[row + 1][col] != 1)
+                        {
+                            count++;
+                                break;
+                        }
+                        else
+                        {
+                            count++;
+                        }
                     }
-                    
-                    else if(check==true && (maze[row][col]==2))
+                    else
                     {
-                        return outCount;
+                        count++;
                     }
-                    outCount = outCount+1;
                 }
             }
-            return outCount;
+            
+            return count;
         }
     }
 
-}
 
-class Solution
-{
-    public static void Main(string[] args)
+    class Program
     {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        int mazeRows = Convert.ToInt32(Console.ReadLine().Trim());
-        int mazeColumns = Convert.ToInt32(Console.ReadLine().Trim());
-
-        List<List<int>> maze = new List<List<int>>();
-
-        for (int i = 0; i < mazeRows; i++)
+        public static void Main(string[] args)
         {
-            maze.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(mazeTemp => Convert.ToInt32(mazeTemp)).ToList());
+           // TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int mazeRows = Convert.ToInt32(Console.ReadLine().Trim());
+            int mazeColumns = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<List<int>> maze = new List<List<int>>();
+
+            for (int i = 0; i < mazeRows; i++)
+            {
+                maze.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(mazeTemp => Convert.ToInt32(mazeTemp)).ToList());
+            }
+
+            int x = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int y = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int result = Result.minMoves(maze, x, y);
+
+            //textWriter.WriteLine(result);
+
+            //textWriter.Flush();
+            //textWriter.Close();
         }
-
-        int x = Convert.ToInt32(Console.ReadLine().Trim());
-
-        int y = Convert.ToInt32(Console.ReadLine().Trim());
-
-        int result = Result.minMoves(maze, x, y);
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
     }
 }
